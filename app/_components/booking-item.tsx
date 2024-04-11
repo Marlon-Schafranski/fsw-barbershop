cb52181f1ba9d26e018e2e8420882cba45c2a5ed
Marlon-Schafranski <marlonschafranski963@gmail.com>
2024-04-11 17:34:13 -0300

    feat: add bokking component

---
+++ app/_components/booking-item.tsx
@@ -0,0 +1,14 @@
+import { Card, CardContent } from "./ui/card";
+
+const BookingItem = () => {
+    return ( 
+        <Card>
+            <CardContent>
+                
+
+            </CardContent>
+        </Card>
+     );
+}
+ 
+export default BookingItem;
